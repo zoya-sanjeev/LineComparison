2a096e780c19a501d63444c8591cc4b77a827715
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 16:45:26 +0530

    UC2 2 lines equal or not

--- src/com/bridgelabz/LineComp/LineComparison.java
+++ src/com/bridgelabz/LineComp/LineComparison.java
@@ -8,7 +8,19 @@ public class LineComparison {
 		double x2 =Math.floor(Math.random()*10);
 		double y1 =Math.floor(Math.random()*10);
 		double y2 =Math.floor(Math.random()*10);
-		System.out.println("Length of line "+ Math.round(Math.sqrt(Math.pow(x2-x1, 2)+ Math.pow(y2-y1, 2))));
+		
+		double a1 =Math.floor(Math.random()*10);
+		double a2 =Math.floor(Math.random()*10);
+		double b1 =Math.floor(Math.random()*10);
+		double b2 =Math.floor(Math.random()*10);
+		
+		String l1=Long.toString(Math.round(Math.sqrt(Math.pow(x2-x1, 2)+ Math.pow(y2-y1, 2))));
+		String l2=Long.toString(Math.round(Math.sqrt(Math.pow(a2-a1, 2)+ Math.pow(b2-b1, 2))));
+		if(l1.equals(l2)) {
+			System.out.println("The Lines are equal");
+		}
+		else
+				System.out.println("The Lines are not equal");
 
 	}
 
