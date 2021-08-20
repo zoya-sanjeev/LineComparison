487037f795083275b72d53d9bdf1953263dd6ad9
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 16:06:30 +0530

    Welcome Message

---
+++ src/com/bridgelabz/LineComp/LineComparison.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.LineComp;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparison");
+
+	}
+
+}
