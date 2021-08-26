38345d76618614e91741b93eb45d7a4246969f96
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-26 22:58:37 +0530

    REFACTOR: implemented interface for compare and equality methods

--- src/com/bridgelabz/LineComp/Point.java
+++ src/com/bridgelabz/LineComp/Point.java
@@ -1,16 +1,42 @@
 package com.bridgelabz.LineComp;
 
+import java.util.Scanner;
+
 public class Point {
-	public double xCoordinate;
-	public double yCoordinate;
-	Point(){
-		this.xCoordinate=0.0;
-		this.yCoordinate=0.0;
-	}
+	private double xCoordinate;
+	private double yCoordinate;
 	
-	Point(double x, double y){
-		this.xCoordinate=x;
-		this.yCoordinate=y;
+	static Scanner sc=new Scanner(System.in);
+	
+	Point(){
+		System.out.println("Enter X and Y coordinates of point");
+		double xCoordinate=sc.nextDouble();
+		double yCoordinate=sc.nextDouble();
+		this.xCoordinate=xCoordinate;
+		this.yCoordinate=yCoordinate;
+	}
+
+	public double getxCoordinate() {
+		return xCoordinate;
+	}
+
+	public void setxCoordinate(double xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public double getyCoordinate() {
+		return yCoordinate;
+	}
+
+	public void setyCoordinate(double yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+ 
+	
+	public double getLengthOfLine( Point endPoint) {
+		double lengthOfLine= Math.sqrt(Math.pow(endPoint.getxCoordinate() -this.getxCoordinate(), 2)+ Math.pow(endPoint.getyCoordinate() -this.getyCoordinate(), 2));
+		return lengthOfLine ;
+		
 	}
 
 }
