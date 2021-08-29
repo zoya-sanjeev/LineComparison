9c6d6be5252b9d41ad983be2285e7a03173ea313
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-29 15:47:36 +0530

    MODIFIED: class variables and methods added

--- src/com/bridgelabz/LineComp/Line.java
+++ src/com/bridgelabz/LineComp/Line.java
@@ -1,5 +1,19 @@
 package com.bridgelabz.LineComp;
 
 public class Line {
-
+	
+	Point startPoint;
+	Point endPoint;
+	
+	Line(Point start, Point end){
+		this.startPoint=start;
+		this.endPoint=end;
+	}
+	
+	double getLengthOfLine(Point startPoint,Point endPoint) {
+		
+		double lengthOfLine= Math.sqrt(Math.pow(endPoint.getxCoordinate() -startPoint.getxCoordinate(), 2)+ Math.pow(endPoint.getyCoordinate() -startPoint.getyCoordinate(), 2));
+		return lengthOfLine ;
+		
+	}
 }
