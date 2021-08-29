7293e931d4262cea0f0d3f75f2a1485a9bd5c60a
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-29 16:25:28 +0530

    REFEACTORED: linecomparison interface and implementation changed according to new line class

--- src/com/bridgelabz/LineComp/CompareLengthsImplementation.java
+++ src/com/bridgelabz/LineComp/CompareLengthsImplementation.java
@@ -3,12 +3,12 @@ package com.bridgelabz.LineComp;
 public class CompareLengthsImplementation implements CompareLengthsInterface{
 	
 	@Override
-	public void equalityOfLines(double line1, double line2) {
-		String lineOne= line1+"";
-		String lineTwo= line2+"";
-		System.out.println("Line one:"+lineOne);
-		System.out.println("Line two:"+lineTwo);
-		if(lineOne.equals(lineTwo)) {
+	public void equalityOfLines(Line line1, Line line2) {
+		Double lengthOfLine1=line1.getLengthOfLine();
+		Double lengthOfLine2=line2.getLengthOfLine();
+		System.out.println("Line one:"+lengthOfLine1);
+		System.out.println("Line two:"+lengthOfLine2);
+		if(lengthOfLine1.equals(lengthOfLine2)) {
 			System.out.println("Line 1 is equal to Line 2");
 		}
 		else {
@@ -18,12 +18,12 @@ public class CompareLengthsImplementation implements CompareLengthsInterface{
 	}
 	
 	@Override
-	public void compareLines(double line1, double line2) {
-		String lineOne= line1+"";
-		String lineTwo= line2+"";
-		System.out.println("Line one:"+lineOne);
-		System.out.println("Line two:"+lineTwo);
-		int compare= lineOne.compareTo(lineTwo);
+	public void compareLines(Line line1, Line line2) {
+		Double lengthOfLine1=line1.getLengthOfLine();
+		Double lengthOfLine2=line2.getLengthOfLine();
+		System.out.println("Line one:"+lengthOfLine1);
+		System.out.println("Line two:"+lengthOfLine2);
+		int compare= lengthOfLine1.compareTo(lengthOfLine2);
 		if(compare>0) {
 			System.out.println("line 1 is greater than line 2");
 		}
